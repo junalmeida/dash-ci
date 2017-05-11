ae8c793f82fb70803a65a237c86283fe81677bd1
Marcos Junior <junalmeida@gmail.com>
2017-05-11 15:13:09 -0300

    Change icon

--- tfs-onpremise-ntlm/MyApplicationContext.cs
+++ tfs-onpremise-ntlm/MyApplicationContext.cs
@@ -8,13 +8,54 @@ namespace TfsOnPremiseNtlm
     public class MyApplicationContext : Swf.ApplicationContext
     {
         private Swf.NotifyIcon trayIconSwf;
+        //private Gtk.StatusIcon trayIcon;
 
         public MyApplicationContext()
         {
             // Initialize Tray Icon
+            //if (Program.IsRunningOnMono())
+            //    TrayGtk();
+            //else
             TraySwf();
         }
 
+        //private void TrayGtk()
+        //{
+        //    Gtk.Application.Init();
+        //    var mem = new MemoryStream();
+        //    Resources.task_manager.Save(mem, ImageFormat.Png);
+        //    mem.Seek(0, SeekOrigin.Begin);
+        //    // Creation of the Icon
+        //    trayIcon = new Gtk.StatusIcon(new Gdk.Pixbuf(mem));
+        //    trayIcon.Visible = true;
+
+        //    // Show/Hide the window (even from the Panel/Taskbar) when the TrayIcon has been clicked.
+        //    trayIcon.Activate += Open;
+        //    // Show a pop up menu when the icon has been right clicked.
+        //    trayIcon.PopupMenu += CreateGtkMenu;
+
+        //    // A Tooltip for the Icon
+        //    trayIcon.TooltipText = Program.Title;
+        //}
+
+        //private void CreateGtkMenu(object o, Gtk.PopupMenuArgs args)
+        //{
+        //    var popupMenu = new Gtk.Menu();
+
+        //    var menuItemOpen = new Gtk.ImageMenuItem("Open");
+        //    menuItemOpen.Image = new Gtk.Image(Gtk.Stock.Open, Gtk.IconSize.Menu);
+        //    popupMenu.Add(menuItemOpen);
+        //    menuItemOpen.Activated += Open;
+
+
+        //    var menuItemQuit = new Gtk.ImageMenuItem("Quit");
+        //    menuItemQuit.Image = new Gtk.Image(Gtk.Stock.Quit, Gtk.IconSize.Menu);
+        //    popupMenu.Add(menuItemQuit);
+        //    menuItemQuit.Activated += Exit;
+
+        //    popupMenu.ShowAll();
+        //    popupMenu.Popup();
+        //}
 
         private void TraySwf()
         {
@@ -39,6 +80,8 @@ namespace TfsOnPremiseNtlm
             // Hide tray icon, otherwise it will remain shown until user mouses over it
             if (trayIconSwf != null)
                 trayIconSwf.Visible = false;
+            //if (trayIcon != null)
+            //    trayIcon.Visible = false;
 
             if (Program.server != null)
                 Program.server.Dispose();
--- tfs-onpremise-ntlm/Properties/Resources.Designer.cs
+++ tfs-onpremise-ntlm/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace TfsOnPremiseNtlm.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap task_manager {
+            get {
+                object obj = ResourceManager.GetObject("task_manager", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
