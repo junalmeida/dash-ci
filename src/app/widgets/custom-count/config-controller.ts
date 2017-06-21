558d3f5d59d43447a7b68b8d361d35688e111e67
Marcos Junior <junalmeida@gmail.com>
2017-06-21 08:55:38 -0300

    Preparing custom count widget.

---
+++ src/app/widgets/custom-count/config-controller.ts
@@ -0,0 +1,62 @@
+﻿namespace DashCI.Widgets.CustomCount {
+
+    export class CustomCountConfigController implements ng.IController {
+        public static $inject = ["$scope", "$mdDialog", "$q", "customResources", "colors", "intervals", "config"];
+        constructor(
+            private $scope: ng.IScope,
+            private $mdDialog: ng.material.IDialogService,
+            private $q: ng.IQService,
+            public customResources: () => Resources.Custom.ICustomResource,
+            public colors: Models.ICodeDescription[],
+            public intervals: Models.IValueDescription[],
+            public vm: ICustomCountData
+        ) { 
+            this.init();
+        }
+
+        private init() {
+            var res = this.customResources();
+            if (!res)
+                return;
+            //res.project_list().$promise
+            //    .then((result: Resources.Custom.IProjectResult) => {
+            //        this.projects = mx(result.value).orderBy(x => x.name).toArray();
+            //    })
+            //    .catch((reason) => {
+            //        console.error(reason);
+            //        this.projects = [];
+            //    });
+
+            //this.$scope.$watch(() => this.vm.project, () => this.getQueries());
+        }
+
+        public queries: Resources.Custom.IQuery[];
+
+
+
+        public getQueries() {
+            var res = this.customResources();
+            if (!res || !this.vm.project)
+                return;
+
+            //var q1 = res.query_list({ project: this.vm.project, folder: "Shared Queries" }).$promise;
+            //var q2 = res.query_list({ project: this.vm.project, folder: "My Queries" }).$promise;
+            //this.$q.all([q1, q2])
+            //    .then((result) => {
+            //        var q = <DashCI.Resources.Custom.IQuery[]>[];
+            //        angular.forEach(result[0].children || result[0].value, (item) => q.push(item));
+            //        angular.forEach(result[1].children || result[1].value, (item) => q.push(item));
+
+            //        this.queries = mx(q).orderBy(x => x.name).toArray();
+            //    }).catch((reason) => {
+            //        console.error(reason);
+            //        this.queries = [];
+            //    });
+
+        }
+
+        public ok() {
+            this.$mdDialog.hide(true);
+        }
+    }
+}
---
+++ src/app/widgets/custom-count/controller.ts
@@ -0,0 +1,149 @@
+﻿namespace DashCI.Widgets.CustomCount {
+
+    export interface ICustomCountData extends Models.IWidgetData {
+        project?: string;
+        poolInterval?: number;
+        queryId?: string;
+        lowerThan?: {
+            value: number;
+            color: string;
+        },
+        greaterThan?: {
+            value: number;
+            color: string;
+        }
+    }
+
+    export class CustomCountController implements ng.IController {
+        public static $inject = ["$scope", "$q", "$timeout", "$interval", "$mdDialog", "customResources"];
+
+        private data: ICustomCountData;
+
+        constructor(
+            private $scope: Models.IWidgetScope,
+            private $q: ng.IQService,
+            private $timeout: ng.ITimeoutService,
+            private $interval: ng.IIntervalService,
+            private $mdDialog: ng.material.IDialogService,
+            private customResources: () => Resources.Custom.ICustomResource
+        ) {
+            this.data = this.$scope.data;
+            this.data.id = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+            this.data.type = Models.WidgetType.CustomCount;
+            this.data.footer = false;
+            this.data.header = true;
+
+            this.$scope.$watch(
+                () => this.$scope.$element.height(),
+                (height: number) => this.sizeFont(height)
+            );
+            this.$scope.$watch(
+                () => this.data.poolInterval,
+                (value: number) => this.updateInterval()
+            );
+            this.$scope.$on("$destroy", () => this.finalize());
+
+            this.init();
+        }
+
+        private handle: ng.IPromise<any>;
+        private finalize() {
+            if (this.handle)
+                this.$interval.cancel(this.handle);
+            console.log("dispose: " + this.data.id + "-" + this.data.title);
+        }
+
+        private init() {
+            this.data.title = this.data.title || "Query";
+            this.data.color = this.data.color || "grey";
+
+            //default values
+            this.data.queryId = this.data.queryId || "";
+            this.data.poolInterval = this.data.poolInterval || 20000;
+            this.updateInterval();
+            this.update();
+        }
+
+        private sizeFont(altura: number) {
+            var p = this.$scope.$element.find("p");
+            var fontSize = Math.round(altura / 1.3) + "px";
+            var lineSize = Math.round((altura) - 60) + "px";
+            p.css('font-size', fontSize);
+            p.css('line-height', lineSize);
+
+            var img = this.$scope.$element.find(".avatar");
+            var size = Math.round(altura - 32);
+            img.width(size);
+            img.height(size);
+        }
+
+        public config() {
+            this.$mdDialog.show({
+                controller: CustomCountConfigController,
+                controllerAs: "ctrl",
+                templateUrl: 'app/widgets/custom-count/config.html',
+                parent: angular.element(document.body),
+                //targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: false,
+                resolve: {
+                    config: () => {
+                        var deferred = this.$q.defer();
+                        this.$timeout(() => deferred.resolve(this.data), 1);
+                        return deferred.promise;
+                    }
+                }
+            });
+            //.then((ok) => this.createWidget(type));
+
+        }
+        public colorClass: string;
+        public queryCount: number;
+        private updateInterval() {
+            if (this.handle)
+                this.$interval.cancel(this.handle);
+            this.handle = this.$interval(() => this.update(), this.data.poolInterval);
+        }
+        private update() {
+            if (!this.data.project || !this.data.queryId)
+                return;
+            var res = this.customResources();
+            if (!res)
+                return;
+
+            console.log("custom query: " + this.data.title);
+            //res.run_query({
+            //    project: this.data.project,
+            //    queryId: this.data.queryId
+            //}).$promise.then((result: Resources.Custom.IRunQueryResult) => {
+            //    var newCount = result.workItems.length;
+
+            //    if (newCount != this.queryCount) {
+            //        this.queryCount = newCount;
+            //        var p = this.$scope.$element.find("p");
+
+            //        p.addClass('changed');
+            //        this.$timeout(() => p.removeClass('changed'), 1000);
+            //    }
+
+            //    if (this.data.lowerThan && !isNaN(this.data.lowerThan.value) && this.data.lowerThan.color) {
+            //        if (this.queryCount < this.data.lowerThan.value)
+            //            this.colorClass = this.data.lowerThan.color;
+            //    }
+            //    if (this.data.greaterThan && !isNaN(this.data.greaterThan.value) && this.data.greaterThan.color) {
+            //        if (this.queryCount > this.data.greaterThan.value)
+            //            this.colorClass = this.data.greaterThan.color;
+            //    }
+
+            //    console.log("end custom query: " + this.data.title);
+            //})
+            //.catch((reason) => {
+            //    this.queryCount = null;
+            //    console.error(reason);
+            //});
+            this.$timeout(() => this.sizeFont(this.$scope.$element.height()), 500);
+        }
+
+    }
+
+}
